a711310ac23531ab35023e0f6f31223fda89087e
GwynethUdani <crystaludani.ii@gmail.com>
2025-10-10 17:20:11 +0800

    New Blue System

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/LoginForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//namespace EmployeeManagementSystem
+//{
+//    public partial class LoginForm : Form
+//    {
+//        public LoginForm()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void InitializeComponent()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public static string fullname, Age, Section;
+//        private object txtUsername;
+//        private object txtPassword;
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            string username = txtUsername.Text.Trim();
+//            string password = txtPassword.Text.Trim();
+
+//            if (username == "" && password == "")
+//            {
+//                MessageBox.Show("Please input username and password");
+//                return;
+//            }
+//            else if (username == "")
+//            {
+//                MessageBox.Show("Please input username");
+//                return;
+//            }
+//            else if (password == "")
+//            {
+//                MessageBox.Show("Please input password");
+//                return;
+//            }
+
+//            string sql = $"SELECT [FullName], [Section], [Age] FROM Users WHERE Username='{username}' AND [Password]='{password}'";
+//            bool isLogin = CRUD.RETRIEVESINGLE(sql);
+
+//            if (isLogin)
+//            {
+//                fullname = CRUD.dt.Rows[0]["FullName"].ToString();
+//                Section = CRUD.dt.Rows[0]["Section"].ToString();
+//                Age = CRUD.dt.Rows[0]["Age"].ToString();
+//                MessageBox.Show("Welcome " + fullname);
+
+//                Form1 newForm1 = new Form1();
+//                newForm1.ShowDialog();
+//            }
+//            else
+//            {
+//                MessageBox.Show("No Data");
+
+//            }
+
+//        }
+//    }
+//}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/frmAddEmployee.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagementSystem
+{
+	public partial class frmAddEmployee : Form
+	{
+		private static string Email;
+        internal string TransactionNo;
+        private object txtSection;
+
+		public frmAddEmployee()
+		{
+			InitializeComponent();
+		}
+
+		public frmAddEmployee(string text)
+		{
+			Text = text;
+		}
+
+		public static string EmailAddress { get; private set; }
+        public static string RequestorEmail { get; private set; }
+
+        private void btnInsertUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtEmpID.Text) ||
+                string.IsNullOrWhiteSpace(txtRequestorName.Text) ||
+                string.IsNullOrWhiteSpace(txtLocalNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtEmailAddress.Text))
+            {
+                MessageBox.Show("Incomplete fields, Enter valid values", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+         
+            bool dtg_addrequestor = false;
+            string EMS_data = string.Empty;
+
+            EMS_data = "SELECT * FROM [tbl_EmployeeData] WHERE [EmployeeNumber] = '" + txtEmpID.Text + "'";
+
+            dtg_addrequestor = CRUD.CRUD.RETRIEVESINGLE(EMS_data);
+
+            if (dtg_addrequestor == true)
+            {
+                DialogResult result = MessageBox.Show(
+                    "This account '" + txtRequestorName.Text + "' already exists. Do you want to update?",
+                    "Already Exists",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Asterisk
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    string updateQuery = "UPDATE [tbl_EmployeeData] SET " +
+                                         "[EmployeeNumber] = '" + txtEmpID.Text + "', " +
+                                         "[RequestorName] = '" + txtRequestorName.Text + "', " +
+                                         "[RequestorEmail] = '" + txtEmailAddress.Text + "', " +
+                                         "[Section] = '" + cmbSection.Text + "', " +
+                                         "[LocalNumber] = '" + txtLocalNumber.Text + "' " +
+                                         "WHERE [ID] = " + frmMasterData.selectedTransaction;
+
+                    CRUD.CRUD.CUD(updateQuery);
+                    MessageBox.Show("Data updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    txtEmpID.Clear();
+                    txtRequestorName.Clear();
+                    txtEmailAddress.Clear();
+                    txtLocalNumber.Clear();
+                    cmbSection.SelectedIndex = -1;
+                    this.Close();
+                }
+            }
+            else
+            {
+                string add_requestor = "INSERT INTO [tbl_EmployeeData] " +
+                                       "([EmployeeNumber],[RequestorName],[RequestorEmail],[Section],[LocalNumber]) " +
+                                       "VALUES ('" + txtEmpID.Text + "','" + txtRequestorName.Text + "','" + txtEmailAddress.Text + "','" + cmbSection.Text + "','" + txtLocalNumber.Text + "')";
+
+                CRUD.CRUD.CUD(add_requestor);
+                MessageBox.Show("Added Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+
+
+
+
+        private void frmAddEmployee_Load(object sender, EventArgs e)
+		{
+            txtRequestorName.Text = frmMasterData.RequestorName;
+            txtEmailAddress.Text = frmMasterData.Email;
+            cmbSection.Text = frmMasterData.Section;
+            txtLocalNumber.Text = frmMasterData.LocalNumber;
+            txtEmpID.Text = frmMasterData.EmployeeID;
+        }
+	}
+}
